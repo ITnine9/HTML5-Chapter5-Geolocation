0fc851b54a35805ce8e9f44c896ce93c481c1232
ITnine9 <35799419+ITnine9@users.noreply.github.com>
2018-02-01 21:22:07 +0800

    Add pin to the map

--- Geo/code.js
+++ Geo/code.js
@@ -9,12 +9,16 @@ function showMap(coords) {
   var googleLatAndLong = new google.maps.LatLng(coords.latitude, coords.longitude);
 
   var mapOptions = {
-    zoom: 10,
+    zoom: 16,
     center: googleLatAndLong,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var mapDiv = document.getElementById("map");
   map = new google.maps.Map(mapDiv, mapOptions);
+
+  var title = "Your Location";
+  var content = "You are here: " + coords.latitude + ", " + coords.longitude;
+  addMarker(map, googleLatAndLong, title, content);
 }
 
 $(document).ready( function getMyLocation() {
@@ -67,4 +71,26 @@ function computeDistance(startCoords, destCoords) {
 function degreesToRadians(degrees) {
 var radians = (degrees * Math.PI)/180;
 return radians;
+}
+
+function addMarker(map, latlong, title, content) {
+  var markerOptions = {
+    position: latlong,
+    map: map,
+    title: title,
+    clickable: true
+  };
+  var marker = new google.maps.Marker(markerOptions);
+
+  var infoWindowOptions = {
+    content: content,
+    position: latlong
+  };
+
+  var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+  google.maps.event.addListener(marker, "click", function() {
+    infoWindow.open(map);
+  });
+
 }
