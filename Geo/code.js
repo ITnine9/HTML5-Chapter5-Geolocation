be4855fd97b49eec2d6ff8c10273c606a9825ee8
ITnine9 <35799419+ITnine9@users.noreply.github.com>
2018-02-01 15:26:47 +0800

    Add diagnostic test

--- Geo/code.js
+++ Geo/code.js
@@ -1,6 +1,6 @@
 $(document).ready( function getMyLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(displayLocation);
+    navigator.geolocation.getCurrentPosition(displayLocation, displayError);
   } else {
     alert("Oops, no geolocation support");
   }
@@ -12,4 +12,18 @@ function displayLocation(position) {
   var longitude = position.coords.longitude;
 
   $("#location").html("You are at Latitude: " + latitude + " Longitude, " + longitude);
+}
+
+function displayError(error) {
+  var errorTypes = {
+    0: "Unknown error",
+    1: "Permission denied by user",
+    2: "Position is not available",
+    3: "Request timed out"
+  };
+  var errorMessage = errorTypes[error.code];
+  if (error.code == 0 || error.code == 2) {
+    errorMessage = errorMessage + " " + error.message;
+  }
+  $("#location").html(errorMessage);
 }
